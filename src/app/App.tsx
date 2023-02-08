d68e71b50a92b53ee7276c316998eee6f6a75134
Mark <yaazzik@gmail.com>
2023-02-08 18:26:58 +0300

    Sidebar added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,8 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/AppRuoter";
 import {Navbar} from "widgets/Navbar";
 import {useTheme} from "app/providers/ThemeProvider";
+import {Sidebar} from "widgets/Sidebar";
+import React from "react";
 
 export const App = () => {
 
@@ -11,7 +13,10 @@ export const App = () => {
   return (
     <div className={classNames('app', {},[theme])}>
       <Navbar />
-      <AppRouter />
+      <div className='content-page'>
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   )
 }
--- src/app/providers/AppRuoter/ui/AppRouter.tsx
+++ src/app/providers/AppRuoter/ui/AppRouter.tsx
@@ -10,8 +10,13 @@ const AppRouter = () => {
           <Route
             key={path}
             path={path}
-            element={element}
-          />))}
+            element={
+            <div className='page-wrapper'>
+              {element}
+            </div>
+            }
+          />
+        ))}
       </Routes>
     </Suspense>
   );
