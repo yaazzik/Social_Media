4e6b70300f9810e2e934314fb2446cb9e0fdd981
Mark <yaazzik@gmail.com>
2023-02-10 16:09:00 +0300

    Lint configs added

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,12 @@
-type Mods = Record<string, string | boolean>
+type Mods = Record<string, boolean | string>
 
-export function classNames(className: string, mods?: Mods, additional?: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
-        className,
+        cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([cls, value]) => Boolean(value))
-            .map(([cls]) => cls),
-    ].join(' ');
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
+    ]
+        .join(' ');
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,12 @@
-import type webpack from 'webpack';
+import webpack from 'webpack';
 import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { type BuildEnv, type BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
-export default (env: BuildEnv): webpack.Configuration => {
+export default (env: BuildEnv) => {
     const paths: BuildPaths = {
-        build: path.resolve(__dirname, 'build'),
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
     };
