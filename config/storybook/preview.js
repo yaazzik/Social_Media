6917eda1801b80fd1fed9cf842dde93d8b48d42a
Mark <yaazzik@gmail.com>
2023-02-16 12:17:03 +0300

    Storybook added, For every component was made his own .stories files

---
+++ config/storybook/preview.js
@@ -0,0 +1,19 @@
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/decorators/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/decorators/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
+import { RouteDecorator } from '../../src/shared/config/storybook/decorators/RouteDecorator';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouteDecorator);
