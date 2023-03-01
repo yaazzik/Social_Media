c54d7a30a413179b532f772b203f0b2b65a70dfa
Mark <yaazzik@gmail.com>
2023-03-01 18:56:05 +0300

    translations fixed

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,17 @@ import 'shared/config/i18n/i18n';
 import 'app/styles/index.scss';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 
 render(
     <StoreProvider>
         <BrowserRouter>
             <ErrorBoundary>
                 <ThemeProvider>
-                    <App />
+                    <I18nextProvider i18n={i18next}>
+                        <App />
+                    </I18nextProvider>
                 </ThemeProvider>
             </ErrorBoundary>
         </BrowserRouter>
