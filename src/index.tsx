1a107dfb3676c2c5641907d950fe8f01960f4d06
Mark <yaazzik@gmail.com>
2023-03-01 19:56:42 +0300

    linting fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,18 @@ import './app/styles/index.scss';
 import { App } from 'app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-import 'shared/config/i18n/i18n';
 import 'app/styles/index.scss';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import { I18nextProvider } from 'react-i18next';
-import i18next from 'i18next';
+import i18n from 'shared/config/i18n/i18n';
 
 render(
     <StoreProvider>
         <BrowserRouter>
             <ErrorBoundary>
                 <ThemeProvider>
-                    <I18nextProvider i18n={i18next}>
+                    <I18nextProvider i18n={i18n}>
                         <App />
                     </I18nextProvider>
                 </ThemeProvider>
